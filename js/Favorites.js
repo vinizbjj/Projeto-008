a0fd5e090d42e7b7a476d850ce8696d6c647c3a9
vinizbjj <viniz.bjj@gmail.com>
2022-09-30 17:33:33 -0300

    projeto finalizado

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,3 +1,7 @@
+import { GithubUser } from "./GithubUser.js";
+
+// classe que vai conter a lógica dos dados
+// como os dados serao estruturados
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
@@ -5,30 +9,50 @@ export class Favorites {
     }
 
     load() {
-        this.entries = [
-            {
-                login: 'maykbrito',
-                name: 'Mayk Brito',
-                public_repos: '76',
-                followers: '120000'
-            },
-            {
-                login: 'diego3g',
-                name: 'Diego Fernanedes',
-                public_repos: '76',
-                followers: '120000'
-            }
-        ]
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
 
+    }
+
+    save() {
+        localStorage.setItem('@github-favorites', JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        try {
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if (userExists) {
+                throw new Error('Usúario já cadastrado')
+            }
+
+
+            const user = await GithubUser.search(username)
+
+            if (user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch (error) {
+            alert(error.message)
+        }
     }
 
     delete(user) {
         const filteresEntries = this.entries.filter(entry => entry.login !== user.login)
 
-        console.log(filteresEntries)
+        this.entries = filteresEntries
+        this.update()
+        this.save()
     }
+
 }
 
+
+/* classe que vai criar a visualização e eventos do HTML */
 export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
@@ -36,6 +60,15 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onadd()
+    }
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
@@ -43,10 +76,11 @@ export class FavoritesView extends Favorites {
 
         this.entries.forEach(user => {
             const row = this.createRow()
-            console.log(row)
+
 
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
             row.querySelector('.user img').alt = `Image de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
@@ -72,7 +106,7 @@ export class FavoritesView extends Favorites {
         tr.innerHTML = `
     
     <td class="user">
-        <img src="https://github.com/vinizbjj.png" alt="">
+        <img src="https://github.com/vinizbjj.png" alt="Imagem do usuario">
         <a href="http://github.com/vinizbjj" target="_blank">
             <p>Vinicius Oliveira</p>
             <span>Vinizbjj</span>
